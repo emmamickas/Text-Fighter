2877a7d1f1b1a99c577bd196f11f53cbbe89896f
Emma <emma.mickas@wsu.edu>
2021-11-16 22:04:47 -0800

    Added tests before changing variable name in Random class to ensure maintenance of functionality.

---
+++ src/com/hotmail/kalebmarc/textfighter/main/RandomTest.java
@@ -0,0 +1,40 @@
+package com.hotmail.kalebmarc.textfighter.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RandomTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRandomInt() {
+		assertTrue(Random.RInt(10) <= 10);
+		
+		assertTrue(Random.RInt(10) >= 0);
+		
+		assertTrue(Random.RInt(5, 10) <= 10);
+		
+		assertTrue(Random.RInt(5, 10) >= 5);
+	}
+
+}
